23088c7a9e944bc363482d3a7a65111bceca7216
Alyssa <alysurr@Alyssas-MacBook-Pro.local>
2021-08-20 02:39:18 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Header from './components/Header/Header.jsx';
+import Video from './components/Video/Video.jsx';
+import Info from './components/Info/Info.jsx';
+import Comments from './components/Comments/Comments.jsx';
+import Sidebar from './components/Sidebar/Sidebar.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <Video />
+    <Info />
+    <Comments />
+    <Sidebar />
+    </>
   );
 }
 
